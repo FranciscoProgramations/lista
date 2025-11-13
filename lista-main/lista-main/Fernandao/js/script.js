2811850ca5673001cbc2f1dfdeaa2028c28cdf4b
Francisco <frasa112009@gmail.com>
2025-11-13 19:06:43 -0300

    Add files via upload

---
+++ lista-main/lista-main/Fernandao/js/script.js
@@ -0,0 +1,71 @@
+document.querySelector("#adicionar").addEventListener("click", adicionarTarefa)
+textoTarefa = document.getElementById("tarefa")
+listaTarefa = document.getElementById("lista")
+document.querySelector("#gravar").addEventListener("click", salvarTarefa)
+
+carregarnoinicio()
+
+//  Criando a Tarefa e adicionando ela na lista
+
+function adicionarTarefa(){
+    tarefa = textoTarefa.value.trim();
+
+    if (tarefa) {
+        criarTarefa(tarefa)
+
+        textoTarefa.value = "";
+
+    }
+}
+
+function criarTarefa(tarefa){
+    var listaItem = document.createElement("li");
+
+    listaItem.textContent = tarefa;
+
+    listaTarefa.appendChild(listaItem)
+
+    function limparTarefas(){
+        localStorage.clear();
+        listaItem.remove();
+    }
+    
+    listaItem.addEventListener('click', function(){
+            listaItem.style.textDecoration = "line-through"
+    })
+
+    listaItem.addEventListener('dblclick', function(){
+            listaTarefa.removeChild(listaItem)
+    })
+
+
+    document.querySelector("#limpar").addEventListener("click", limparTarefas)
+}
+
+
+// Salvando no localstorage
+
+function salvarTarefa(){
+{   let Tarefas=[];
+    listaTarefa.querySelectorAll("li").forEach(function(listaItem) {
+        Tarefas.push(listaItem.textContent.trim());
+    })
+
+    localStorage.setItem('tarefas', JSON.stringify(Tarefas))
+}
+}
+
+
+// Carregando o localstorage no início (nome autoexplicativo)
+
+function carregarnoinicio(){
+
+    tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
+    tarefas.forEach(criarTarefa);
+
+}
+
+
+
+
+
